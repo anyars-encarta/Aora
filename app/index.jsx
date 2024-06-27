1df3f2b0c8e36423f93491e5f8db94c7d2ba68b5
anyars-encarta <anyarsencarta@gmail.com>
2024-06-27 13:10:13 +0000

    Add assets and constants

--- app/index.jsx
+++ app/index.jsx
@@ -5,7 +5,7 @@ import { Link } from 'expo-router';
 const App = () => {
   return (
     <View className="flex-1 items-center justify-center bg-white">
-      <Text className='text-3xl'>Aora!</Text>
+      <Text className='text-3xl font-pblack'>Aora!</Text>
       <StatusBar style="auto" />
       <Link href='/profile' style={{ color: 'blue'}}>Go to Profile</Link>
     </View>
