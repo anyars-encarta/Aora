64f77dd2941fac50dc97d661f15d9b78c1e64f3c
anyars-encarta <anyarsencarta@gmail.com>
2024-06-29 17:11:43 +0000

    Add logo and image to onboarding page

--- app/index.jsx
+++ app/index.jsx
@@ -1,14 +1,28 @@
 import { StatusBar } from 'expo-status-bar';
-import { Text, View } from 'react-native';
+import { Text, View, ScrollView, Image } from 'react-native';
 import { Link } from 'expo-router';
+import { SafeAreaView } from 'react-native-safe-area-context';
+import { images } from '../constants'
 
 const App = () => {
   return (
-    <View className="flex-1 items-center justify-center bg-white">
-      <Text className="text-3xl font-pblack">Aora!</Text>
-      <StatusBar style="auto" />
-      <Link href='/home' style={{color: 'blue'}}>Go to Home</Link>
-    </View>
+    <SafeAreaView className='bg-primary h-full'>
+      <ScrollView contentContainerStyle={{ height: '100%' }}>
+        <View className='w-full items-center h-full px-4'>
+          <Image 
+          source={images.logo} 
+          className='w-[130px] h-[84px]'
+          resizeMode='contain'
+          />
+
+          <Image 
+          source={images.cards} 
+          className='max-w-[380px] w-full h-[300px]'
+          resizeMode='contain'
+          />
+        </View>
+      </ScrollView>
+    </SafeAreaView>
   );
 }
 
