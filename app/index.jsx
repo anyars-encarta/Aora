d7f4036c1aac053b433ed5ab2f7dfa84d186708b
anyars-encarta <anyarsencarta@gmail.com>
2024-06-29 18:14:21 +0000

    Add router to button to navigate to auth page

--- app/index.jsx
+++ app/index.jsx
@@ -1,6 +1,6 @@
 import { StatusBar } from 'expo-status-bar';
 import { Text, View, ScrollView, Image } from 'react-native';
-import { Link } from 'expo-router';
+import { Redirect, router } from 'expo-router';
 import { SafeAreaView } from 'react-native-safe-area-context';
 import { images } from '../constants'
 import CustomButton from '../components/CustomButton';
@@ -9,7 +9,7 @@ const App = () => {
   return (
     <SafeAreaView className='bg-primary h-full'>
       <ScrollView contentContainerStyle={{ height: '100%' }}>
-        <View className='w-full items-center h-full px-4'>
+        <View className='w-full items-center min-h-[85vh] px-4'>
           <Image
             source={images.logo}
             className='w-[130px] h-[84px]'
@@ -39,7 +39,7 @@ const App = () => {
 
           <CustomButton
             title='Continue with Email'
-            handlePress={() => { }}
+            handlePress={() => router.push('/sign-in')}
             containerStyles='w-full mt-7'
           />
         </View>
