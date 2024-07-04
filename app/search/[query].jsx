fe1c47d56a884decf74e0191f831f6e9ebc40005
anyars-encarta <anyarsencarta@gmail.com>
2024-07-04 08:42:20 +0000

    Redirect search from homepage to query page

--- app/search/[query].jsx
+++ app/search/[query].jsx
@@ -1,10 +1,13 @@
 import { View, Text } from 'react-native'
 import React from 'react'
+import { useLocalSearchParams } from 'expo-router'
 
 const Search = () => {
+  const { query } = useLocalSearchParams();
+
   return (
     <View>
-      <Text>Search</Text>
+      <Text className='text-3xl text-white'>{query}</Text>
     </View>
   )
 }
