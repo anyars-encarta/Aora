5f02a5a376b4a697f84a433a369fc414089cd06c
anyars-encarta <anyarsencarta@gmail.com>
2024-07-05 17:36:36 +0000

    Implement post searching

--- app/search/[query].jsx
+++ app/search/[query].jsx
@@ -1,14 +1,58 @@
-import { View, Text } from 'react-native'
-import React from 'react'
-import { useLocalSearchParams } from 'expo-router'
+import { View, Text, FlatList, Image, RefreshControl, Alert } from 'react-native';
+import React, { useEffect, useState } from 'react';
 import { SafeAreaView } from 'react-native-safe-area-context';
+import { images } from '../../constants';
+import SearchInput from '../../components/SearchInput';
+import Trending from '../../components/Trending';
+import EmptyState from '../../components/EmptyState';
+import useAppwrite from '../../lib/useAppwrite';
+import { searchPosts } from '../../lib/appwrite';
+import VideoCard from '../../components/VideoCard';
+import { useLocalSearchParams } from 'expo-router';
 
 const Search = () => {
   const { query } = useLocalSearchParams();
+  const { data: posts, refetch } = useAppwrite(() => searchPosts(query));
+
+  const [refreshing, setRefreshing] = useState(false);
+
+  console.log(query, posts)
+  useEffect(() => {
+    refetch()
+  }, [query])
+
+  console.log('The posts in search: ', posts);
 
   return (
     <SafeAreaView className='bg-primary h-full'>
-      <Text className='text-3xl text-white'>{query}</Text>
+      <FlatList
+        data={posts}
+        keyExtractor={(item) => item.$id}
+        renderItem={({ item }) => (
+          <VideoCard
+            video={item}
+          />
+        )}
+        ListHeaderComponent={() => (
+          <View className='my-6 px-4'>
+            <Text className='font-pmedium text-sm text-gray-100'>
+              Search Results
+            </Text>
+            <Text className='text-2xl font-psemibold text-white'>{query}</Text>
+
+            <View className='mt-6 mb-8'>
+              <SearchInput initialQuery={query} />
+            </View>
+          </View>
+        )}
+
+        ListEmptyComponent={() => (
+          <EmptyState
+            title='No Videos Found'
+            subtitle='No videos found for this search query'
+          />
+        )}
+      />
     </SafeAreaView>
   )
 }
--- components/SearchInput.jsx
+++ components/SearchInput.jsx
@@ -4,10 +4,10 @@ import React, { useState } from 'react';
 import { icons } from '../constants';
 import { router, usePathname } from 'expo-router';
 
-const SearchInput = () => {
+const SearchInput = ({ initialQuery }) => {
     // const [showPassword, setShowPassword] = useState(false);
     const pathname = usePathname();
-    const [query, setQuery] = useState('');
+    const [query, setQuery] = useState(initialQuery || '');
 
     return (
         <View className='border-2 border-black-200 w-full h-16 px-4 bg-black-100 rounded-2xl focus:border-secondary items-center flex-row space-x-4'>
