a1a04a3d1bdbd6be7a9965a565ddcaa5a05c0467
anyars-encarta <anyarsencarta@gmail.com>
2024-07-05 17:52:38 +0000

    Remove unused calls and states

--- app/search/[query].jsx
+++ app/search/[query].jsx
@@ -1,9 +1,7 @@
-import { View, Text, FlatList, Image, RefreshControl, Alert } from 'react-native';
-import React, { useEffect, useState } from 'react';
+import { View, Text, FlatList } from 'react-native';
+import React, { useEffect } from 'react';
 import { SafeAreaView } from 'react-native-safe-area-context';
-import { images } from '../../constants';
 import SearchInput from '../../components/SearchInput';
-import Trending from '../../components/Trending';
 import EmptyState from '../../components/EmptyState';
 import useAppwrite from '../../lib/useAppwrite';
 import { searchPosts } from '../../lib/appwrite';
@@ -13,8 +11,6 @@ import { useLocalSearchParams } from 'expo-router';
 const Search = () => {
   const { query } = useLocalSearchParams();
   const { data: posts, refetch } = useAppwrite(() => searchPosts(query));
-
-  const [refreshing, setRefreshing] = useState(false);
 
   console.log(query, posts)
   useEffect(() => {
