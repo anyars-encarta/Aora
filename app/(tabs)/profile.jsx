f58976fbd81e0f276916d1226c78c1c1248ad755
anyars-encarta <anyarsencarta@gmail.com>
2024-07-06 16:26:40 +0000

    Complete profile page

--- app/(tabs)/profile.jsx
+++ app/(tabs)/profile.jsx
@@ -1,23 +1,20 @@
-import { View, Text, FlatList } from 'react-native';
-import React, { useEffect } from 'react';
+import { View, FlatList, TouchableOpacity, Image } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
-import SearchInput from '../../components/SearchInput';
 import EmptyState from '../../components/EmptyState';
 import useAppwrite from '../../lib/useAppwrite';
-import { searchPosts } from '../../lib/appwrite';
+import { getUserPosts } from '../../lib/appwrite';
 import VideoCard from '../../components/VideoCard';
-import { useLocalSearchParams } from 'expo-router';
+import { useGlobalContext } from '../../context/GlobalProvider';
+import { icons } from '../../constants';
+import InfoBox from '../../components/InfoBox';
 
 const Profile = () => {
-  const { query } = useLocalSearchParams();
-  const { data: posts, refetch } = useAppwrite(() => searchPosts(query));
+  const { user, setUser, setIsLoggedIn } = useGlobalContext();
+  const { data: posts, refetch } = useAppwrite(() => getUserPosts(user.$id));
 
-  console.log(query, posts)
-  useEffect(() => {
-    refetch()
-  }, [query])
+  const logout = () => {
 
-  console.log('The posts in search: ', posts);
+  };
 
   return (
     <SafeAreaView className='bg-primary h-full'>
@@ -30,14 +27,37 @@ const Profile = () => {
           />
         )}
         ListHeaderComponent={() => (
-          <View className='my-6 px-4'>
-            <Text className='font-pmedium text-sm text-gray-100'>
-              Search Results
-            </Text>
-            <Text className='text-2xl font-psemibold text-white'>{query}</Text>
+          <View className='w-full justify-center items-center mt-6 mb-12 px-4'>
+            <TouchableOpacity
+              className='w-full items-end mb-10'
+              onPress={logout}
+            >
+              <Image source={icons.logout} resizeMode='contain' className='w-6 h-6' />
+            </TouchableOpacity>
 
-            <View className='mt-6 mb-8'>
-              <SearchInput initialQuery={query} />
+            <View className='w-16 h-16 border border-secondary rounded-lg justify-center items-center'>
+              <Image source={{ uri: user?.avatar }} className='w-[90%] h-[90%] rounded-lg' resizeMode='cover' />
+            </View>
+
+            <InfoBox
+              title={user?.username}
+              containerStyles='mt-5'
+              titleStyles='text-lg'
+            />
+
+            <View className='mt-5 flex-row'>
+              <InfoBox
+                title={posts.length || 0}
+                subtitle='Posts'
+                containerStyles='mr-10'
+                titleStyles='text-xl'
+              />
+
+              <InfoBox
+                title='15.6K'
+                subtitle='Followers'
+                titleStyles='text-xl'
+              />
             </View>
           </View>
         )}
